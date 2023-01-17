331350b4e47963368f14fb44fe3baf8e7f828fcc
Jungmoo Yoon <alexjm@naver.com>
2023-01-17 17:10:34 +0900

    8.1

--- Day4_mission.py
+++ Day4_mission.py
@@ -1,19 +1,8 @@
-#7.10
+#8.1
 
-start1 = ["fee", "fie","foe"]
-rhymes = [
-    ("flop","get a mop"),
-    ("fope","turn the rope"),
-    ("fa", " get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog","walk the dog"),
-    ("fun","say we're done")
-
-]
-start2= "Someone better"
-
-for i,j in rhymes:
-    print(f"{start1[0]}! {start1[1]}! {start1[2]}! {i}!")
-    print(f"{j} {start2}!")
+dic ={
+    "dog" : "chien",
+    "cat" : "chat,",
+    "walrus" : "morse"
+}
 
