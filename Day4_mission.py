f0095e609380092fc80f79d780e5660ab0beffe2
Jungmoo Yoon <alexjm@naver.com>
2023-01-17 17:08:44 +0900

    7.11

--- Day4_mission.py
+++ Day4_mission.py
@@ -1,11 +1,19 @@
 #7.10
 
-even = []
-for number in range(10):
-    if number % 2 == 0:
-        even.append(number)
-    else:
-        continue
+start1 = ["fee", "fie","foe"]
+rhymes = [
+    ("flop","get a mop"),
+    ("fope","turn the rope"),
+    ("fa", " get your ma"),
+    ("fudge", "call the judge"),
+    ("fat", "pet the cat"),
+    ("fog","walk the dog"),
+    ("fun","say we're done")
 
-print(even)
+]
+start2= "Someone better"
+
+for i,j in rhymes:
+    print(f"{start1[0]}! {start1[1]}! {start1[2]}! {i}!")
+    print(f"{j} {start2}!")
 
